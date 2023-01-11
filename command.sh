b671ef08d691985fef9b3c13052c65e8a942f536
Maranga <rchdmaranga@gmail.com>
2023-01-11 15:52:50 +0300

    commands used while doing the task

---
+++ command.sh
@@ -0,0 +1,15 @@
+# https://github.com/marangaa/my-app https://github.com/marangaa/
+npx create-react-app my-app
+cd my-app
+npm start
+
+gh repo create
+
+git checkout -b update_logo
+
+git add .
+git commit -m ""
+
+gh pr create
+
+
